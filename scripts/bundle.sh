d3e6fd75df3edd9347df4ae28546c65bc9548ba7
SainathLatkar <latkarsainath@gmail.com>
2026-03-26 10:39:58 +0530

    Make versions depend on a single file VERSION
    Previously we had to make version changes at multiple places, with this change we need to change a single file and version gets changed for all project wherever necessary

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -6,4 +6,8 @@ echo "Bundling OpenAPI..."
 npx redocly bundle openapi/root.yaml \
   -o docs/bundled.yaml
 
+# Inject version from VERSION file into source and bundled yaml
+sed -i '' "s/  version: .*/  version: $VERSION/" openapi/root.yaml
+sed -i '' "s/  version: .*/  version: $VERSION/" docs/bundled.yaml
+
 echo "Bundled spec created"
